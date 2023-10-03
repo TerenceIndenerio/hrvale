5ff3e915d25c6ec1fd75d71842cbdc96c353537b
mazi39662 <94585959+CypherStudio@users.noreply.github.com>
2023-10-03 11:47:41 +0800

    Added Asset Folder for Fonts & for later use

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Fonts */
+import './assets/font.css';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router)
