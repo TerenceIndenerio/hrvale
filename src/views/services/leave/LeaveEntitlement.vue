e0f6d491f6ff4cd68006e827396227deaeec99af
mazi39662 <94585959+CypherStudio@users.noreply.github.com>
2023-10-25 11:33:57 +0800

    Leave Entitlement: Select Option api Fix

--- src/views/services/leave/LeaveEntitlement.vue
+++ src/views/services/leave/LeaveEntitlement.vue
@@ -4,67 +4,58 @@
       <ion-content :fullscreen="true">
         <Refresher />
         <div v-if="showComponent">
-  
-            <ion-card class="card-round">
-                <div class="flex-c">
-                <p class="username-text"><strong>{{ employeeName }}</strong></p>
-                </div>
-                <div class="flex-h flex-c">
-                <p class="leave-text">Leave Period: </p>
-                <p class="leave-text-val"><strong>{{ leaveEntitlementFor }}</strong></p>
-                </div>
-            </ion-card>
 
-            <ion-card class="select-date-container card-round">
-                <ion-select v-model="selectedApi">
-                    <div slot="label" class="">Select Date:</div>
-                    <ion-select-option value="api1">2023-01-01 - 2023-08-31</ion-select-option>
-                    <ion-select-option value="api2">2023-09-01 - 2024-08-31</ion-select-option>
-                    <!-- <ion-select-option value="api3">2024-09-01 - 2025-08-31</ion-select-option> This is still empty --> 
-                </ion-select>
-            </ion-card>
-          
-          <ion-card class="result-container">
-            <div v-for="(entitlement, index) in entitlements" :key="index">
-              <LeaveEntitlementCard
-                :leaveType="entitlement.leaveType.name"
-                :entitlement="entitlement.entitlement"
-                :daysUsed="entitlement.daysUsed"
-                :creditedDate="entitlement.creditedDate"
-              />
-            </div>
+          <ion-card class="select-date-container card-round">
+            <ion-select v-model="selectedApi">
+              <div slot="label">Select Date:</div>
+                <ion-select-option
+                  v-for="(option, index) in leavePeriodOptions"
+                  :key="index"
+                  :value="option.value"
+                >
+                  {{ option.text }}
+                </ion-select-option>
+              </ion-select>
           </ion-card>
+          
+          <div v-for="(entitlement, index) in entitlements" :key="index">
+            <LeaveEntitlementCard
+              :leaveType="entitlement.leaveType.name"
+              :entitlement="entitlement.entitlement"
+              :daysUsed="entitlement.daysUsed"
+              :creditedDate="entitlement.creditedDate"
+            />
+          </div>
+          
         </div>
       </ion-content>
     </ion-page>
 </template>
   
 <script>
-import { IonPage, IonHeader, IonContent, IonCard, IonIcon, IonSelect, IonSelectOption } from '@ionic/vue'
-import HeaderReturn from '@/components/header/HeaderReturn.vue'
-import LeaveEntitlementCard from '@/views/services/leave/components/LeaveEntitlementCard.vue'
-import Refresher from '@/components/refresher/Refresher.vue'
-import { defineComponent } from 'vue'
+import { IonPage, IonCard, IonSelect, IonSelectOption, IonContent } from '@ionic/vue';
+import HeaderReturn from '@/components/header/HeaderReturn.vue';
+import LeaveEntitlementCard from '@/views/services/leave/components/LeaveEntitlementCard.vue';
+import Refresher from '@/components/refresher/Refresher.vue';
+import { defineComponent, ref, onMounted, watch } from 'vue';
 import { useStore } from 'vuex';
-import axios from 'axios'
+import axios from 'axios';
 
 export default defineComponent({
   name: 'LeaveEntitlements',
   components: {
     IonPage,
-    IonHeader,
-    IonContent,
     IonCard,
-    IonIcon,
     HeaderReturn,
     LeaveEntitlementCard,
     Refresher,
     IonSelect,
-    IonSelectOption
+    IonSelectOption,
+    IonContent
   },
   setup() {
     return {
-        store: useStore(),
+      store: useStore(),
     }
   },
   data() {
@@ -72,27 +63,14 @@ export default defineComponent({
       showComponent: false,
       entitlements: [],
       headerTitle: 'Leave Entitlements',
-      employeeName: '',
-      leaveEntitlementFor: '',
-      selectedApi: 'api1',
-    }
-  },
-  watch: {
-    selectedApi: 'fetchData',
+      selectedApi: null,
+      leavePeriodOptions: [],
+    };
   },
   methods: {
-    async fetchData() {
+    async fetchLeavePeriods() {
       try {
-        this.store.commit('loader/updateLoader', true);
-        const apiUrls = {
-          api1: 'https://hrp-staging-delta.bapplware.com/web/index.php/api/v2/leave/leave-entitlements?limit=50&offset=0&fromDate=2023-09-01&toDate=2024-08-31',
-          api2: 'https://hrp-staging-delta.bapplware.com/web/index.php/api/v2/leave/leave-entitlements?limit=50&offset=0&fromDate=2023-01-01&toDate=2023-08-31',
-        //   api3: 'https://hrp-staging-delta.bapplware.com/web/index.php/api/v2/leave/leave-entitlements?limit=50&offset=0&fromDate=2024-09-01&toDate=2025-08-31', This is still Empty
-        };
-
-        const apiUrl = apiUrls[this.selectedApi];
-        
-        const response = await axios.post(
+        const tokenResponse = await axios.post(
           'https://hrp-staging-delta.bapplware.com/web/index.php/auth/token',
           {
             clientId: 'test_id',
@@ -100,42 +78,89 @@ export default defineComponent({
             userId: 1
           }
         );
-        const token = response.data.token;
+        const token = tokenResponse.data.token;
+
+        const currentLeavePeriodResponse = await axios.get(
+          'https://hrp-staging-delta.bapplware.com/web/index.php/api/v2/leave/leave-periods',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+        
+
+        const { startDate, endDate } = currentLeavePeriodResponse.data.meta.currentLeavePeriod;
+        this.selectedApi = `${startDate} - ${endDate}`;
+        this.makeGetRequest(startDate, endDate);
+
+        const response = await axios.get(
+          'https://hrp-staging-delta.bapplware.com/web/index.php/api/v2/leave/leave-periods',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+
+        const leavePeriods = response.data.data;
+        this.leavePeriodOptions = leavePeriods.map((period, index) => ({
+          value: `${period.startDate} - ${period.endDate}`,
+          text: `${period.startDate} - ${period.endDate}`,
+        }));
+        this.showComponent = true;
+
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async makeGetRequest(fromDate, toDate) {
+      try {
+        this.store.commit('loader/updateLoader', true);
+        const apiUrl = `https://hrp-staging-delta.bapplware.com/web/index.php/api/v2/leave/leave-entitlements?limit=50&offset=0&fromDate=${fromDate}&toDate=${toDate}`;
+        const tokenResponse = await axios.post(
+          'https://hrp-staging-delta.bapplware.com/web/index.php/auth/token',
+          {
+            clientId: 'test_id',
+            clientSecret: 'test_secret',
+            userId: 1
+          }
+        );
+        const token = tokenResponse.data.token;
 
         const headers = {
           Authorization: `Bearer ${token}`
         };
         const dataResponse = await axios.get(apiUrl, { headers });
 
-        this.employeeName = `${dataResponse.data.data[0].employee.firstName} ${dataResponse.data.data[0].employee.middleName || ''} ${dataResponse.data.data[0].employee.lastName}`;
-        this.leaveEntitlementFor = `${dataResponse.data.meta.fromDate} - ${dataResponse.data.meta.toDate}`;
-
         this.entitlements = dataResponse.data.data.map((entitlement) => {
-          const leaveData = {
+          return {
             leaveType: entitlement.leaveType,
             entitlement: entitlement.entitlement,
             daysUsed: entitlement.daysUsed,
             creditedDate: entitlement.creditedDate,
           };
-          return leaveData;
         });
+        this.store.commit('loader/updateLoader', false);
       } catch (error) {
+        this.store.commit('loader/updateLoader', false);
         console.error(error);
       }
-      this.store.commit('loader/updateLoader', false);
     },
   },
-  async created() {
-    const data = await this.fetchData();
-    setTimeout(() => {
-      this.showComponent = true;
-    }, 1000);
-    this.store.commit('loader/updateLoader', false);
+  watch: {
+  selectedApi(newVal) {
+    const selectedOption = this.leavePeriodOptions.find((option) => option.value === newVal);
+    if (selectedOption) {
+      const fromDate = selectedOption.text.split(' - ')[0];
+      const toDate = selectedOption.text.split(' - ')[1];
+      this.makeGetRequest(fromDate, toDate);
+    }
+  },
+},
+  async mounted() {
+    await this.fetchLeavePeriods();
   },
 });
 </script>
-
-  
 
 <style scoped>
   p {
@@ -160,13 +185,9 @@ export default defineComponent({
     font-size: 30px;
     color: black;
   }
-  .result-container {
-    margin-top: 20px;
-    border-radius: 20px;
-  }
   .card-round {
     border-radius: 20px;
-    width: fit-content;
+    width: 90%;
     margin: 10px auto;
   }
   .username-text, .leave-text {
